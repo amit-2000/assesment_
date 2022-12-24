55140ace8c3a7d5f33006f04769388d90f4123ba
amit <amitperane8473@gmail.com>
2022-12-24 14:09:07 +0530

    Home icon --done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme, Box } from '@chakra-ui/react';
 // import Home from './Components/Home';
 import Module from './Components/Module';
 function App() {
   return (
     <ChakraProvider theme={theme} resetCSS={false}>
       {/* <Home /> */}
-      <Module />
+      <Box >
+        <Module />
+      </Box>
     </ChakraProvider>
   );
 }
--- src/Components/Module.jsx
+++ src/Components/Module.jsx
@@ -1,51 +1,80 @@
 import React from 'react';
-import { Box, Divider, Image, Text } from '@chakra-ui/react';
-
+import { Box, Image, Text, Button } from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { IconContext } from 'react-icons';
+import { MdHome } from 'react-icons/md';
 const Module = () => {
   return (
-    <Box
-      display={{ base: 'inline-block', lg: 'flex' }}
-      width="100%"
-      m={'0 auto'}
-      p={0}
-    >
-      <Box width={{ base: '100%', lg: '60%' }} height={'90vh'} m={'20px auto'}>
-        <Image
-          borderRadius={10}
-          fit={'cover'}
-          boxSize={'100%'}
-          src="https://images.unsplash.com/photo-1644620990884-0e6e8743f71a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
-        ></Image>
-      </Box>
+    <Box>
       <Box
-        width={{ base: '90vw', lg: '40%' }}
-        height={'80vh'}
-        pos={{ base: 'absolute', lg: 'relative' }}
-        top={{ base: 6 }}
-        display={{ base: 'flex' }}
-        alignItems={{ base: 'center' }}
-        justifyContent={{ base: 'center' }}
+        textAlign={'right'}
+        mr={8}
+        mt={5}
+        cursor="pointer"
+        position={'relative'}
+        zIndex={200}
       >
-        <Box width={'300px'} textAlign={{ base: 'center', lg: 'left' }}>
-          <Box cursor={'pointer'}>
-            <Text>Module One</Text>
-            <Divider />
-          </Box>
-          <Box cursor={'pointer'}>
-            <Text>Module Two</Text>
-            <Divider />
-          </Box>
-          <Box cursor={'pointer'}>
-            <Text>Module Three</Text>
-            <Divider />
-          </Box>
-          <Box cursor={'pointer'}>
-            <Text>Module Four</Text>
-            <Divider />
-          </Box>
-          <Box cursor={'pointer'}>
-            <Text>Module Five</Text>
-            <Divider />
+        <IconContext.Provider value={{ size: '2.2em', color: 'gray' }}>
+          <MdHome />
+        </IconContext.Provider>
+      </Box>
+      <Box display={{ base: 'inline-block', lg: 'flex' }} width="100%" p={0}>
+        <Box
+          width={{ base: '100%', lg: '60%' }}
+          height={'90vh'}
+          mt={-10}
+          opacity={{ base: 0.6, lg: 1 }}
+        >
+
+          <Image
+            borderRadius={10}
+            fit={'cover'}
+            boxSize={'100%'}
+            src="https://images.unsplash.com/photo-1644620990884-0e6e8743f71a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+          ></Image>
+        </Box>
+        <Box
+          width={{ base: '95%', lg: '40%' }}
+          height={'80vh'}
+          pos={{ base: 'absolute', lg: 'relative' }}
+          top={{ base: 6 }}
+          display={{ base: 'flex' }}
+          alignItems={{ base: 'center' }}
+          justifyContent={{ base: 'center' }}
+        >
+          <Box
+            width={'300px'}
+            textAlign={{ base: 'center', lg: 'left' }}
+            color="blue.500"
+            fontWeight={600}
+          >
+            <Box cursor={'pointer'} borderBottom={'2px solid #b3b4b5'}>
+              <Text>Module One</Text>
+            </Box>
+            <Box cursor={'pointer'} borderBottom={'2px solid #b3b4b5'}>
+              <Text>Module Two</Text>
+            </Box>
+            <Box cursor={'pointer'} borderBottom={'2px solid #b3b4b5'}>
+              <Text>Module Three</Text>
+            </Box>
+            <Box cursor={'pointer'} borderBottom={'2px solid #b3b4b5'}>
+              <Text>Module Four</Text>
+            </Box>
+            <Box cursor={'pointer'} borderBottom={'2px solid #b3b4b5'}>
+              <Text>Module Five</Text>
+            </Box>
+            <Button
+              borderRadius={'full'}
+              mt={20}
+              colorScheme="facebook"
+              variant="solid"
+              p={5}
+              w={'100%'}
+              cursor='pointer'
+            >
+              Select Module
+              {<ChevronRightIcon ml={'10px'} boxSize={'20px'} />}
+            </Button>
           </Box>
         </Box>
       </Box>
